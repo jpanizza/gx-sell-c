4a6db19e21d4dd4acb5c79c91ae82583b51f5ff5
Jose Paz <jpaz@li-d6b0314c-2914-11b2-a85c-d1dcaa9a0adc.ibm.com>
2021-08-24 12:54:51 -0300

    changes map

--- image-map.tf
+++ image-map.tf
@@ -1,5 +1,13 @@
 locals {
         image_map = {
-			"us-south" = "r006-0bb26deb-651f-449c-9842-3d28b837edae"
+			"us-south" = "r006-f9d5b112-7a81-49a3-a59b-54cdb2af6901"
+			"us-east" = "r014-498410ce-ca7b-43af-bba8-7a0923fc2554"
+			"eu-gb" = "r018-2b06a9b6-429d-4ccc-84c6-d2e3e80b830f"
+			"eu-de" = "r010-d9026a2c-eed7-4e3c-b374-d57c8bc5bc5e"
+			"jp-tok" = "r022-787dc9e6-ee42-49e6-81b1-b5b4ddacd3f5"
+			"jp-osa" = "r034-8d0057cc-7e37-44cb-8948-92ed51ba4248"
+			"au-syd" = "r026-73fdff33-8b75-4a03-b50e-9926735f43b4"
+			"ca-tor" = "r038-ba3c25e7-c226-4c1c-8409-9bc8d14751e5"
+                        "br-sao" = "r042-f51f36f9-c0d6-4a69-b242-7ab33c58ef04"
         }
 }
