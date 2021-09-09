c614aa9e3bc514fb70b0d3672965d38220e9214d
José Paz <jpaz17@hotmail.com>
2021-09-09 17:18:31 -0300

    Update image-map.tf

--- image-map.tf
+++ image-map.tf
@@ -3,7 +3,7 @@ locals {
 			"us-south" = "r006-0c8cf958-80bc-4cc2-9402-6dc4f56cdaaa"
 			"us-east" = "r014-731840a9-9353-403a-8d42-39dcce13392d"
 			"eu-gb" = "r018-c3180ca8-893b-432e-be11-c648b888a3a0"
-			"eu-de" = "r010-11900b33-8221-4707-9ce0-bcc112c7868a"
+			"eu-de" = "r010-b350de3b-10c1-4197-b04f-3ce62cfea7c2"
 			"jp-tok" = "r022-509a072b-03b1-4921-a291-40ee9b622894"
 			"jp-osa" = "r034-728d4f77-c024-41e5-9188-51695736cca1"
 			"au-syd" = "r026-d40dcb46-fb7f-4b7e-8cd9-52fee852257e"
