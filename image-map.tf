a515009a498af7595c2a8f02d0edb7467176b554
José Lamas <jlr@genexus.com>
2021-10-03 20:18:13 -0300

    Update image-map.tf

--- image-map.tf
+++ image-map.tf
@@ -1,13 +1,13 @@
 locals {
         image_map = {
 			"us-south" = "r006-19888b4c-62ef-4896-924d-e7dde998269d"
-			"us-east" = "r014-731840a9-9353-403a-8d42-39dcce13392d"
-			"eu-gb" = "r018-c3180ca8-893b-432e-be11-c648b888a3a0"
-			"eu-de" = "r010-b350de3b-10c1-4197-b04f-3ce62cfea7c2"
-			"jp-tok" = "r022-509a072b-03b1-4921-a291-40ee9b622894"
-			"jp-osa" = "r034-728d4f77-c024-41e5-9188-51695736cca1"
-			"au-syd" = "r026-d40dcb46-fb7f-4b7e-8cd9-52fee852257e"
-			"ca-tor" = "r038-b9e94d9a-588a-45fc-9e04-ef8e3d024338"
-                        "br-sao" = "r042-41dc93a4-db54-402d-9635-b35ae5e9aad7"
+			"us-east" = "r014-275711a4-1dca-44d6-964e-5088ef22a0b7"
+			"eu-gb" = "r018-42b49e52-c5fc-4b57-be51-11982c8c9003"
+			"eu-de" = "r010-a089c5da-d975-4a8f-a51e-43298cf9c829"
+			"jp-tok" = "r022-aaada966-edfa-4af0-9255-3072f74091cc"
+			"jp-osa" = "r034-140ff745-7a7d-4d0f-88d3-65e09970fc8a"
+			"au-syd" = "r026-3f50cd53-1a4c-427c-a5d8-8d42c25552a7"
+			"ca-tor" = "r038-43542f8b-f743-4cdb-9d66-c9bc0f25b2d4"
+			"br-sao" = "r042-39013e15-228f-4c96-a4f0-5a874c57a85a"
         }
 }
